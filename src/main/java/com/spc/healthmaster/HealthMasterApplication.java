14290ea91f0e1197bb3122c49ec3195aa64f6e3c
wilson pereira melendez <wilson.pereira@mercadolibre.com.co>
2023-05-10 17:39:29 -0500

    commit initial

---
+++ src/main/java/com/spc/healthmaster/HealthMasterApplication.java
@@ -0,0 +1,13 @@
+package com.spc.healthmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthMasterApplication.class, args);
+    }
+
+}
