17eb915d92f450fb9149dbfc5edc34b83a357b4f
wspereira <wspereira@outlook.es>
2023-05-27 11:13:44 -0500

    Deshabilitando btn try it out de swagger, en el property de prod hay que ponerlo true para deshabilitar

--- src/main/java/com/spc/healthmaster/configuration/SwaggerConfig.java
+++ src/main/java/com/spc/healthmaster/configuration/SwaggerConfig.java
@@ -1,14 +1,22 @@
 package com.spc.healthmaster.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer{
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Value("${swagger.disabledBtn}")
+    private boolean disabledBtn;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -18,6 +26,15 @@ public class SwaggerConfig implements WebMvcConfigurer{
                         .description("Mediante la API proporcionada, puedes realizar diversas acciones en tus aplicaciones de manera programática. " +
                                 "Esto incluye iniciar y detener aplicaciones o grupos de aplicaciones según sea necesario, así como obtener el estado actual de cada una de ellas")
                         .version("1.0.0"));
+    }
+
+    @Primary
+    @Bean
+    public SwaggerUiConfigProperties swaggerUiConfigProperties() {
+        SwaggerUiConfigProperties properties = new SwaggerUiConfigProperties();
+        properties.setDefaultModelRendering("example");
+        properties.setSupportedSubmitMethods(disabledBtn ? Arrays.asList(new String[]{}) : null);
+        return properties;
     }
 
     @Override
